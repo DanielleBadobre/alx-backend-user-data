176908c52840337281277bd36c2c2a9a75849cef
Danielle Badobre <badobredanielle@gmail.com>
2024-11-14 03:41:36 +0000

    task 3 documentation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,18 +7,26 @@ from flask import List, TypeVar
 
 class Auth:
     """class for authentication
+        Return:
+            a
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require_auth
+            Return:
+                False
         """
         return False
 
     def authorization_header(self, request=None) -> str:
         """ authorization_header
+            Return:
+                None
         """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ current_user
+            Return:
+                None
         """
         return None
