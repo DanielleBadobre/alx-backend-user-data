3c69b15f2520b2ac7666a9644fe934bede5f277c
Danielle Badobre <badobredanielle@gmail.com>
2024-11-15 02:28:22 +0000

    task 5 is description the problem

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -44,15 +44,15 @@ def before_request():
     """ Before the request
     """
     if auth is None:
-        return
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
+        pass
+    else:
+        excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                        '/api/v1/forbidden/']
+        if auth.require_auth(request.path, excluded_paths):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
 
 
 if __name__ == "__main__":
